33af98b899ee1bf24e8fc0f34b14fdfaec70f38d
haygrouve <haygrouve@gmail.com>
2023-09-28 11:20:19 +0300

    fixing cache issue on landing page

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../prismaClient";
 
 export const GET = async (req: Request) => {
+  console.log(req.url);
   const recipes = await prisma.recipe.findMany();
   return new Response(JSON.stringify(recipes));
 };
